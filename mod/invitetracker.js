e0cdd54db831d29a30158c33b3f5eef615e3ee86
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-14 08:03:28 -0400

    Create Invitetracker module

---
+++ mod/invitetracker.js
@@ -0,0 +1,15 @@
+module.exports = async client => {
+  const invites = {};
+  client.on("inviteCreate", async invite => {
+    invites[invite.guild.id].push(invite);
+  });
+
+  client.on("inviteDelete", async invite => {
+    invites[invite.guild.id] = invites[invite.guild.id].filter(i => i.code != invite.code);
+  });
+
+  client.on("guildMemberAdd", async member => {
+    let g = member.guild;
+    if (!invites[g.id]) invites[g.id] = await g.fetchInvites();
+  });
+};

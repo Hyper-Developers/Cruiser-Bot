1c6f8613332a0b0ad2c1d55b8d0152f13e86b707
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-14 08:54:52 -0400

    Refactor & add new settings

--- mod/settingscommand.js
+++ mod/settingscommand.js
@@ -1,57 +1,42 @@
 module.exports = async (client) => {
   client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) return;
-    if (interaction.commandName === "cruiser") {
-      if (interaction.options.getSubcommandGroup() === "settings") {
-        if (
-          !interaction.member ||
-          !(
-            interaction.member.id == interaction.guild.ownerId ||
-            interaction.member.permissions.has("ADMINISTRATOR")
-          )
-        ) {
-          return await interaction.reply({
-            content: "You do not have the ADMINISTRATOR permission!",
-            ephemeral: true,
-          });
-        }
-        switch (interaction.options.getSubcommand()) {
-          case "autoslowmode":
-            if (interaction.options.get("target") == null) {
-              await client.targetRatelimits60s.delete(interaction.channelId);
-            } else {
-              await client.targetRatelimits60s.set(
-                interaction.channelId,
-                interaction.options.get("target").value
-              );
-            }
-            break;
-          case "autolockdown":
-            if (interaction.options.get("threshold") == null) {
-              await client.maximumRatelimits3s.delete(interaction.channelId);
-            } else {
-              await client.maximumRatelimits3s.set(
-                interaction.channelId,
-                interaction.options.get("threshold").value
-              );
-            }
-            break;
-          case "virustotal":
-            if (interaction.options.get("apikey") == null) {
-              await client.virustotalApikeys.delete(interaction.guild.id);
-            } else {
-              await client.virustotalApikeys.set(
-                interaction.guild.id,
-                interaction.options.get("apikey").value
-              );
-            }
-            break;
-        }
-        return await interaction.reply({
-          content: "Set option succesfully.",
-          ephemeral: true,
-        });
-      }
+    if (!interaction.isCommand() || interaction.commandName != "cruiser" && interaction.options.getSubcommandGroup() != "settings") return;
+    if (
+      !interaction.member ||
+      !(
+        interaction.member.id == interaction.guild.ownerId ||
+        interaction.member.permissions.has("ADMINISTRATOR")
+      )
+    )
+      return await interaction.reply({
+        content: "You do not have the ADMINISTRATOR permission!",
+        ephemeral: true,
+      });
+    const settings = {
+      "autoslowmode": [client.targetRatelimits60s, interaction.channelId, "target"],
+      "autolockdown": [client.maximumRatelimits3s, interaction.channelId, "threshold"],
+      "virustotal": [client.virustotalApikeys, interaction.guild.id, "apikey"],
+      "ksoft": [client.enableKsoft, interaction.guild.id, "enabled"],
+      "discordrep": [client.enableDrep, interaction.guild.id, "enabled"],
+      "antibot": [client.enableAntibot, interaction.guild.id, "enabled"],
+      "antiwebhook": [client.enableAntiwebhook, interaction.guild.id, "enabled"],
+    };
+    const thesetting = settings[interaction.options.getSubcommand()];
+    if (interaction.options.get(thesetting[2]) == null){
+      await thesetting[0].delete(thesetting[1]);
+      return await interaction.reply({
+        content: "Unset option `"+interaction.options.getSubcommand()+"` succesfully.",
+        ephemeral: true,
+      });
+    } else {
+      await thesetting[0].set(
+        interaction.channelId,
+        interaction.options.get(thesetting[2]).value
+      );
+      return await interaction.reply({
+        content: "Set option `"+interaction.options.getSubcommand()+"` to `"+interaction.options.get(thesetting[2]).value+"` successfully.",
+        ephemeral: true,
+      });
     }
   });
 };

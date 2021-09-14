977e11e26ed87e69da79dd7dc9c12ab49bfd2106
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-14 08:14:54 -0400

    Beautify & rename

--- mod/settingscommand.js
+++ mod/settingscommand.js
@@ -16,18 +16,18 @@ module.exports = async client => {
             break;
           case "autolockdown":
             if (interaction.options.get("threshold") == null){
-                                                  await client.maximumRatelimits3s.delete(interaction.channelId);
-                                          } else {
-                                                  await client.maximumRatelimits3s.set(interaction.channelId, interaction.options.get("threshold").value);
-                                          }
+              await client.maximumRatelimits3s.delete(interaction.channelId);
+            } else {
+              await client.maximumRatelimits3s.set(interaction.channelId, interaction.options.get("threshold").value);
+            }
             break;
           case "virustotal":
-                                          if (interaction.options.get("apikey") == null){
-                                                  await client.virustotalApikeys.delete(interaction.guild.id);
-                                          } else {
-                                                  await client.virustotalApikeys.set(interaction.guild.id, interaction.options.get("apikey").value);
-                                          }
-                                          break;
+            if (interaction.options.get("apikey") == null){
+              await client.virustotalApikeys.delete(interaction.guild.id);
+            } else {
+              await client.virustotalApikeys.set(interaction.guild.id, interaction.options.get("apikey").value);
+            }
+            break;
         }
         return await interaction.reply({ content: 'Set option succesfully.', ephemeral: true });
       }

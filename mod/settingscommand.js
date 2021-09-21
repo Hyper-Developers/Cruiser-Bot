17d0d996e26f2276991d606a65fa0ff8f39ad5e2
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-21 08:21:28 -0400

    Update settingscommand.js

--- mod/settingscommand.js
+++ mod/settingscommand.js
@@ -1,9 +1,7 @@
 module.exports = async (client) => {
   client.on("interactionCreate", async (interaction) => {
     if (
-      !interaction.isCommand() ||
-      (interaction.commandName != "cruiser" &&
-        interaction.options.getSubcommandGroup() != "settings")
+      !interaction.isCommand() || interaction.commandName != "cruiser" || interaction.options.getSubcommandGroup(false) != "settings"
     )
       return;
     if (

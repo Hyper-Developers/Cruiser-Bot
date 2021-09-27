0a6931aabeec6f617ab2d1cc2ef17cf7186b43c1
Pandapip1 <Pandapip1@users.noreply.github.com>
2021-09-27 11:50:43 +0000

    Prettified Code!

--- mod/heartbeatcommand.js
+++ mod/heartbeatcommand.js
@@ -1,4 +1,3 @@
-
 module.exports = async (client) => {
   client.on("interactionCreate", async (interaction) => {
     if (!interaction.isCommand()) return;
@@ -17,14 +16,44 @@ module.exports = async (client) => {
           content: "You do not have the ADMINISTRATOR permission!",
           ephemeral: true,
         });
-      let guildWarns = ["VirusTotal Anti-Virus and Anti-Scam is disabled.", "IPQualityScore Anti-Scam is disabled.", "KSoft.si global bans are disabled.", "DiscordRep global bans are disabled.", "Cruiser anti-selfbot is disabled.", "Cruiser anti-webhook abuse is disabled."];
-      let guildWarnsEnabled = (await Promise.all([client.virustotalApikeys, client.ipqsApikeys, client.enableKsoft, client.enableDrep, client.enableAntibot, client.enableAntiwebhook].map(db => db.get(interaction.guild.id)))).map(e => !e);
-      if (!guildWarnsEnabled.some(e=>e))
+      let guildWarns = [
+        "VirusTotal Anti-Virus and Anti-Scam is disabled.",
+        "IPQualityScore Anti-Scam is disabled.",
+        "KSoft.si global bans are disabled.",
+        "DiscordRep global bans are disabled.",
+        "Cruiser anti-selfbot is disabled.",
+        "Cruiser anti-webhook abuse is disabled.",
+      ];
+      let guildWarnsEnabled = (
+        await Promise.all(
+          [
+            client.virustotalApikeys,
+            client.ipqsApikeys,
+            client.enableKsoft,
+            client.enableDrep,
+            client.enableAntibot,
+            client.enableAntiwebhook,
+          ].map((db) => db.get(interaction.guild.id))
+        )
+      ).map((e) => !e);
+      if (!guildWarnsEnabled.some((e) => e))
         return await interaction.reply({
           content: "<:good:881629715419516958> Everything is good!",
           ephemeral: true,
         });
-      let result = "<:warning:881629456039571537> There are potential issues with your configuration:\n"+guildWarnsEnabled.filter((e, i) => guildWarnsEnabled[i]).map((e, i, o) => (i == o.length - 1 ? "<:Reply:1234>" : "<:Reply_Continued:1234>")+" "+e).join("\n");
+      let result =
+        "<:warning:881629456039571537> There are potential issues with your configuration:\n" +
+        guildWarnsEnabled
+          .filter((e, i) => guildWarnsEnabled[i])
+          .map(
+            (e, i, o) =>
+              (i == o.length - 1
+                ? "<:Reply:1234>"
+                : "<:Reply_Continued:1234>") +
+              " " +
+              e
+          )
+          .join("\n");
       return await interaction.reply({
         content: result,
         ephemeral: true,

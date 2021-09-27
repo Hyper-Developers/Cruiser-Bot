642d2f0f3826a960a65578be635161962b0c274d
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-27 13:55:37 -0400

    There's a library for that too™️

---
+++ mod/backups.js
@@ -0,0 +1,28 @@
+const backup = require("discord-backup");
+
+module.exports = async (client) => {
+  return; // Disable in development
+  client.on("interactionCreate", async (interaction) => {
+    if (
+      !interaction.isCommand() ||
+      interaction.commandName != "cruiser" ||
+      interaction.options.getSubcommandGroup(false) != "backup"
+    )
+      return;
+    if (
+      !interaction.member ||
+      !(
+        interaction.member.id == interaction.guild.ownerId ||
+        interaction.member.permissions.has("ADMINISTRATOR")
+      )
+    )
+      return await interaction.reply({
+        content: "You do not have the ADMINISTRATOR permission!",
+        ephemeral: true,
+      });
+    return await interaction.reply({
+      content: "NOT IMPLEMENTED",
+      ephemeral: true,
+    });
+  });
+};

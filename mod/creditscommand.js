5a6381f3bffe1315a03d218f0ba5612747ca394c
Pandapip1 <Pandapip1@users.noreply.github.com>
2021-09-27 18:06:23 +0000

    Prettified Code!

--- mod/creditscommand.js
+++ mod/creditscommand.js
@@ -6,16 +6,17 @@ module.exports = async (client) => {
       interaction.options.getSubcommand() == "credits"
     ) {
       return await interaction.reply({
-        content: ""+
-          "Electro Cloud: Hyper Development Owner\n"+
-          "Pandapip1: Main Developer\n"+
-          "Androz2091: Invite Tracking & Backups\n"+
-          "Discord.js: The library that Cruiser uses\n"+
-          "Ksoft.si: Global Bans\n"+
-          "DiscordRep: More Global Bans\n"+
-          "VirusTotal: Virus and Link Scans\n"+
+        content:
+          "" +
+          "Electro Cloud: Hyper Development Owner\n" +
+          "Pandapip1: Main Developer\n" +
+          "Androz2091: Invite Tracking & Backups\n" +
+          "Discord.js: The library that Cruiser uses\n" +
+          "Ksoft.si: Global Bans\n" +
+          "DiscordRep: More Global Bans\n" +
+          "VirusTotal: Virus and Link Scans\n" +
           "IPQualityScore: Faster Link Scans",
-        ephemeral: true
+        ephemeral: true,
       });
     }
   });

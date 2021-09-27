100a6138e82238e27845f9ce46a24aebcd8fcb02
Pandapip1 <Pandapip1@users.noreply.github.com>
2021-09-27 17:53:20 +0000

    Prettified Code!

--- mod/invitetracker.js
+++ mod/invitetracker.js
@@ -1,21 +1,17 @@
-const InvitesTracker = require('@androz2091/discord-invites-tracker');
+const InvitesTracker = require("@androz2091/discord-invites-tracker");
 // A LOT OF CREDIT GOES TO https://github.com/TheShadowGamer/Invite-Manager
 module.exports = async (client) => {
   return; // disable while in development
   const tracker = InvitesTracker.init(client, {
-      fetchGuilds: true,
-      fetchVanity: true,
-      fetchAuditLogs: true
+    fetchGuilds: true,
+    fetchVanity: true,
+    fetchAuditLogs: true,
   });
-  tracker.on('guildMemberAdd', (member, type, invite) => {
-      if (type === 'normal'){
-          
-      } else if(type === 'vanity'){
-          
-      } else if(type === 'unknown'){
-          
-      } else if(type === 'permissions'){
-          
-      }
+  tracker.on("guildMemberAdd", (member, type, invite) => {
+    if (type === "normal") {
+    } else if (type === "vanity") {
+    } else if (type === "unknown") {
+    } else if (type === "permissions") {
+    }
   });
 };

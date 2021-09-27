6c43de82def15a9103b2356c5d0f9b1cc12ebc8d
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-27 07:28:46 -0400

    Add ping command

---
+++ mod/pingcommand.js
@@ -0,0 +1,14 @@
+module.exports = async (client) => {
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+    if (
+      interaction.commandName == "cruiser" &&
+      interaction.options.getSubcommand() == "ping"
+    ) {
+      return await interaction.reply({
+        content: `Ping: \`${client.ws.ping} ms\``,
+        ephemeral: true
+      });
+    }
+  });
+};

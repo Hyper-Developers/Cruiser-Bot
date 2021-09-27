c660f511998c69cc3d41ce842a52a91e443a8bf6
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-27 07:50:24 -0400

    Add heartbeat command

---
+++ mod/heartbeatcommand.js
@@ -0,0 +1,34 @@
+
+module.exports = async (client) => {
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+    if (
+      interaction.commandName == "cruiser" &&
+      interaction.options.getSubcommand() == "heartbeat"
+    ) {
+      if (
+        !interaction.member ||
+        !(
+          interaction.member.id == interaction.guild.ownerId ||
+          interaction.member.permissions.has("ADMINISTRATOR")
+        )
+      )
+        return await interaction.reply({
+          content: "You do not have the ADMINISTRATOR permission!",
+          ephemeral: true,
+        });
+      let guildWarns = ["VirusTotal Anti-Virus and Anti-Scam is disabled.", "IPQualityScore Anti-Scam is disabled.", "KSoft.si global bans are disabled.", "DiscordRep global bans are disabled.", "Cruiser anti-selfbot is disabled.", "Cruiser anti-webhook abuse is disabled."];
+      let guildWarnsEnabled = (await Promise.all([client.virustotalApikeys, client.ipqsApikeys, client.enableKsoft, client.enableDrep, client.enableAntibot, client.enableAntiwebhook].map(db => db.get(interaction.guild.id)))).map(e => !e);
+      if (!guildWarnsEnabled.some(e=>e))
+        return await interaction.reply({
+          content: "<:good:881629715419516958> Everything is good!",
+          ephemeral: true,
+        });
+      let result = "<:warning:881629456039571537> There are potential issues with your configuration:\n"+guildWarnsEnabled.filter((e, i) => guildWarnsEnabled[i]).map((e, i, o) => (i == o.length - 1 ? "<:Reply:1234>" : "<:Reply_Continued:1234>")+" "+e).join("\n");
+      return await interaction.reply({
+        content: result,
+        ephemeral: true,
+      });
+    }
+  });
+};

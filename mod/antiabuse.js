2d82e962cd0082787f4671dae8e7b9de14bf65c0
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-28 07:59:25 -0400

    Add switch

--- mod/antiabuse.js
+++ mod/antiabuse.js
@@ -44,7 +44,7 @@ module.exports = async (client) => {
     });
   });
   client.on("roleUpdate", async (oldRole, role) => {
-    return;
+    if (!(await client.enableInvitetracking.get(role.guild.id))) return;
     if (role.members.length / role.guild.members.length < 0.2) return;
     let newperms = role.permissions;
     await Promise.all(

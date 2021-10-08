c16651322767c7fdd9726ad00b9184a9aa0534cd
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-10-08 11:22:02 -0400

    Create log settings command

---
+++ mod/logsettingscommand.js
@@ -0,0 +1,25 @@
+module.exports = async (client) => {
+  client.on("interactionCreate", async (interaction) => {
+    if (
+      !interaction.isCommand() ||
+      interaction.commandName != "cruiser" ||
+      interaction.options.getSubcommandGroup(false) != "logs"
+    )
+      return;
+    if (
+      !interaction.member ||
+      !(
+        interaction.member.id == interaction.guild.ownerId ||
+        interaction.member.permissions.has("ADMINISTRATOR")
+      )
+    )
+      return await interaction.reply({
+        content: "You do not have the ADMINISTRATOR permission!",
+        ephemeral: true,
+      });
+    return await interaction.reply({
+      content: "NOT IMPLEMENTED",
+      ephemeral: true,
+    });
+  });
+};

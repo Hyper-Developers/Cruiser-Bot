2be9e6ec25df597ef8c6a08db7ddfbf1161062e5
Pandapip1 <Pandapip1@users.noreply.github.com>
2021-10-11 13:15:34 +0000

    Prettified Code!

--- mod/invitetracker.js
+++ mod/invitetracker.js
@@ -24,30 +24,30 @@ module.exports = async (client) => {
   });
   return;
   const invites = {};
-  client.on('ready', async () => {
+  client.on("ready", async () => {
     await client.guilds.fetch();
-    client.guilds.cache.forEach(async g => {
+    client.guilds.cache.forEach(async (g) => {
       const guildInvites = await g.fetchInvites();
       invites[g.id] = guildInvites;
     });
   });
-  client.on('inviteCreate', async invite => {
+  client.on("inviteCreate", async (invite) => {
     const guildInvites = await invite.guild.fetchInvites();
     invites[guild.id] = guildInvites;
   });
-  client.on('inviteDelete', async invite => {
+  client.on("inviteDelete", async (invite) => {
     const guildInvites = await invite.guild.fetchInvites();
     invites[guild.id] = guildInvites;
   });
-  client.on('guildCreate', async invite => {
+  client.on("guildCreate", async (invite) => {
     const guildInvites = await invite.guild.fetchInvites();
     invites[guild.id] = guildInvites;
   });
-  client.on('guildMemberAdd', async member => {
+  client.on("guildMemberAdd", async (member) => {
     const guildInvites = await member.guild.fetchInvites();
     const ei = invites[member.guild.id];
     invites[member.guild.id] = guildInvites;
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const invite = guildInvites.find((i) => ei.get(i.code).uses < i.uses);
     const inviter = client.users.cache.get(invite.inviter.id);
   });
 };

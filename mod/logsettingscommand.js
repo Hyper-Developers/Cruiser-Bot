cbc03f98095d147fed7ea977e65b1086717c4032
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-10-23 12:41:43 -0400

    Add ability to set log channels

--- mod/logsettingscommand.js
+++ mod/logsettingscommand.js
@@ -10,15 +10,40 @@ module.exports = async (client) => {
       !interaction.member ||
       !(
         interaction.member.id == interaction.guild.ownerId ||
-        interaction.member.permissions.has("ADMINISTRATOR")
+        (interaction.member.permissions.has("MANAGE_CHANNELS") && interaction.member.permissions.has("VIEW_AUDIT_LOG"))
       )
     )
       return await interaction.reply({
-        content: "You do not have the ADMINISTRATOR permission!",
+        content: "You do not have the required permissions!",
         ephemeral: true,
       });
+    const thesetting = client[interaction.options.getString("type")];
+    if (interaction.options.getChannel("channel") == null || interaction.options.getChannel("channel").type != "GUILD_TEXT") {
+      await thesetting.delete(interaction.guild.id);
+      return await interaction.reply({
+        content:
+          "Unset log channel for `" +
+          interaction.options.getString("type") +
+          "` succesfully.",
+        ephemeral: true,
+      });
+    } else {
+      await thesetting.set(
+        interaction.guild.id,
+        interaction.options.getChannel("channel").id
+      );
+      return await interaction.reply({
+        content:
+          "Set log channel for `" +
+          interaction.options.getString("type") +
+          "` to <#" +
+          interaction.options.getChannel("channel").id +
+          "> successfully.",
+        ephemeral: true,
+      });
+    }
     return await interaction.reply({
-      content: "NOT IMPLEMENTED",
+      content: "An error occured",
       ephemeral: true,
     });
   });
